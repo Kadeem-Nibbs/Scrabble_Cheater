7ad9db8a401fe862a3268e2015abfbe23a05606b
Kadeem-Nibbs <kadeemnibbs@gmail.com>
2017-12-05 18:21:45 -0500

    more frontend prototyping

--- scrabble_cheater/server.py
+++ scrabble_cheater/server.py
@@ -1,10 +1,21 @@
 from flask import Flask, request, render_template, redirect
+from flask_socketio import SocketIO
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
 
-@app.before_request
-def before_request():
-    pass
-
+# @app.before_request
+# def before_request():
+#     pass
+#
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@socketio.on('my event')
+def handle_my_custom_event(json):
+    print 'received json:', str(json)
+
+if __name__ == "__main__":
+    socketio.run(app)
