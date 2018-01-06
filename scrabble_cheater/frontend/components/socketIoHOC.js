fce2192cd3d8b956704104953e78e716daf2ce41
Alex Hays <alexhays@gmail.com>
2018-01-05 20:25:53 -0600

    small cleanup - need to add .env file and loader

--- scrabble_cheater/frontend/components/socketIoHOC.js
+++ scrabble_cheater/frontend/components/socketIoHOC.js
@@ -5,8 +5,13 @@ export default (WrappedComponent) => {
   class socketIoHOC extends Component {
     constructor(props) {
       super(props)
-      // TODO: Move this in .env at some point
-      this.socket = io()
+
+      if (process.env.NODE_ENV === 'development') {
+        this.socket = io('http://localhost:4000')
+      } else {
+        // Production uses server.js :: proxies websockets
+        this.socket = io()
+      }
 
       this.state = {
         suggestedWords: null
--- scrabble_cheater/frontend/server.js
+++ scrabble_cheater/frontend/server.js
@@ -3,8 +3,10 @@ var proxy = require('http-proxy-middleware')
 let app = express()
 let port = 80
 
-// NODE_ENV=local npm run server
-if(process.env.NODE_ENV === 'local') {
+// Run from terminal: 
+// `NODE_ENV=development npm run serve`
+// Use `yarn dev` server for developing: this is to make sure production build works locally
+if(process.env.NODE_ENV === 'development') {
   port = 3000
 }
 
