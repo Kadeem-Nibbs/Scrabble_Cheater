5f3a36ecf851031527b3a9d6362a839e0236d04f
Alex Hays <alexhays@gmail.com>
2018-01-26 12:21:44 -0500

    make work locally better

--- scrabble_cheater/frontend/server.js
+++ scrabble_cheater/frontend/server.js
@@ -4,20 +4,29 @@ let proxy = require('http-proxy-middleware')
 let app = express()
 let https = require('https')
 let fs = require('fs')
-let port = 80
 
-// Run from terminal: 
-// `NODE_ENV=local_production_test npm run serve`
-// Use `yarn dev` server for developing: this is to make sure production build works locally
+let port
+let sslPath
+let options
+
+// To test  from terminal: 
+// To test the production build locally run:
+//  `NODE_ENV=local_production_test npm run serve`
+//  Must have ran `yarn build` first.
+// Use `yarn dev` server for developing
 if(process.env.NODE_ENV === 'local_production_test') {
   port = 3000
+  sslPath = ''
+  options = {}
+} else {
+   port = 80
+   sslPath = '/etc/letsencrypt/live/www.wordswithfiends.com/'
+   options = {
+      key: fs.readFileSync(sslPath + 'privkey.pem'),
+      cert: fs.readFileSync(sslPath + 'fullchain.pem')
+  }
 }
 
-const sslPath = '/etc/letsencrypt/live/www.wordswithfiends.com/'
-const options = {
-    key: fs.readFileSync(sslPath + 'privkey.pem'),
-    cert: fs.readFileSync(sslPath + 'fullchain.pem')
-}
 
 app.use('/dist', express.static('dist'))
 app.use(express.static(__dirname + '/dist'))
@@ -32,7 +41,9 @@ let wsProxy = proxy('/', {
 app.use(wsProxy)
 app.listen(port)
 app.on('upgrade', wsProxy.upgrade)
-let server = https.createServer(options, app).listen(443)
 
+if(process.env.NODE_ENV !== 'local_production_test') {
+  https.createServer(options, app).listen(443)
+}
 
-console.log('R U N N I N G')
+console.log('Why hello there')
