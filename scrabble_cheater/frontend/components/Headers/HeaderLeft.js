fb32d6e7d010e17b0022b2c4a13cb377a39c1c5b
Alex Hays <alexhays@gmail.com>
2018-02-02 14:44:14 -0500

    change title sizes

--- scrabble_cheater/frontend/components/Headers/HeaderLeft.js
+++ scrabble_cheater/frontend/components/Headers/HeaderLeft.js
@@ -4,10 +4,10 @@ import { Header } from 'semantic-ui-react'
 class HeaderLeft extends Component {
   render() {  
     return(
-      <Header as='h2'>
-        Get answers for Words with Friends!
+      <Header as='h3'>
+        Get answers for Words with Friends or Scrabble!
         <Header.Subheader>
-           <h3>Click on a tile below, select a direction, and enter a word.</h3>
+           <h4>Click on a tile below, select a direction, and enter a word.</h4>
         </Header.Subheader>
       </Header>
     )
